43d0838f93e7c00d674a12796f0b883729925f62
1179676540 <40532206+1179676540@users.noreply.github.com>
2018-11-11 18:46:43 +0800

    修改场景,怪物，npc读入方式为xml，便于对游戏内容进行修改

--- src/main/java/rpg/area/Area.java
+++ src/main/java/rpg/area/Area.java
@@ -14,20 +14,20 @@ import rpg.pojo.Npc;
 public class Area {
 	//public static LinkedList<Npc> npcList =new LinkedList<Npc>();
 	public static LinkedList<Scene> sceneList =new LinkedList<Scene>();
-	private static HashMap<String, Integer> mp1 = new HashMap<String, Integer>();
-	static int[][] mp2 = new int[30][30];
-	static {
-		mp1.put("起始之地", 1);
-		mp1.put("村子", 2);
-		mp1.put("森林", 3);
-		mp1.put("城堡", 4);
-		mp2[1][2]=1;
-		mp2[2][1]=1;
-		mp2[2][3]=1;
-		mp2[2][4]=1;
-		mp2[3][2]=1;
-		mp2[4][2]=1;
-	}
+	public static HashMap<String, Integer> mp1 = new HashMap<String, Integer>();
+	public static int[][] mp2 = new int[30][30];
+//	static {
+//		mp1.put("起始之地", 1);
+//		mp1.put("村子", 2);
+//		mp1.put("森林", 3);
+//		mp1.put("城堡", 4);
+//		mp2[1][2]=1;
+//		mp2[2][1]=1;
+//		mp2[2][3]=1;
+//		mp2[2][4]=1;
+//		mp2[3][2]=1;
+//		mp2[4][2]=1;
+//	}
 
 	public static int checkArea(String msg, int idfrom) {
 		Integer id = mp1.get(msg);
--- src/main/java/rpg/server/Main.java
+++ src/main/java/rpg/server/Main.java
@@ -1,9 +1,17 @@
 package rpg.server;
 
+import java.io.File;
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.scheduling.SchedulingException;
 
 import rpg.area.Area;
 import rpg.area.Scene;
@@ -13,11 +21,86 @@ import rpg.pojo.Skill;
 import rpg.skill.SkillList;
 
 public class Main {
-	public static void main(String[] args) {
-		init();
+	public static void main(String[] args) throws Exception {
+		ArrayList<Npc> npcList = new ArrayList<Npc>();
+		ArrayList<Monster> monsterList = new ArrayList<Monster>();
+		initMonster(monsterList);
+		initNpc(npcList);
+		initScene(monsterList, npcList);
+//		init();
 		ApplicationContext context = new ClassPathXmlApplicationContext("classpath*:server.xml");
 		ServerMain serverMain = (ServerMain) context.getBean("serverMain");
 		serverMain.run();
+	}
+
+	// 初始化怪物
+	private static void initMonster(ArrayList<Monster> monsterList) throws Exception {
+		SAXReader sr = new SAXReader();
+		Document document = sr.read(new File("src\\main\\java\\rpg.conf\\monster.xml"));
+		Element root = document.getRootElement();
+		List<Element> elementList = root.elements();
+		for (Element e : elementList) {
+			Monster monster = new Monster();
+			monster.setName(e.elementText("name"));
+			monster.setAliveFlag(true);
+			monster.setHp(Integer.valueOf(e.elementText("hp")));
+			monster.setAck(Integer.valueOf(e.elementText("ack")));
+			monsterList.add(monster);
+		}
+	}
+
+	// 初始化npc
+	public static void initNpc(ArrayList<Npc> npcList) throws Exception {
+		SAXReader sr = new SAXReader();
+		Document document = sr.read(new File("src\\main\\java\\rpg.conf\\npc.xml"));
+		Element root = document.getRootElement();
+		List<Element> elementList = root.elements();
+		for (Element e : elementList) {
+			Npc npc = new Npc();
+			npc.setName(e.elementText("name"));
+			npc.setMsg(e.elementText("msg"));
+			npcList.add(npc);
+		}
+	}
+
+	// 初始化场景
+	public static void initScene(ArrayList<Monster> monsterList, ArrayList<Npc> npcList) throws Exception {
+		SAXReader sr = new SAXReader();
+		Document document = sr.read(new File("src\\main\\java\\rpg.conf\\Scene.xml"));
+		Element root = document.getRootElement();
+		List<Element> elementList = root.elements();
+		List<Scene> sceneList = new ArrayList();
+		for (Element e : elementList) {
+			Scene scene = new Scene();
+			scene.setName(e.elementText("name"));
+			scene.setId(Integer.valueOf(e.elementText("id")));
+			// 设置npc
+			String[] npcId = e.elementText("npc").split(",");
+			LinkedList<Npc> list = new LinkedList<Npc>();
+			for (String npc : npcId) {
+				Integer id = Integer.valueOf(npc);
+				Npc npc2 = npcList.get(id - 1);
+				list.add(npc2);
+			}
+			scene.setNpcList(list);
+			// 设置怪物
+			String[] monsterId = e.elementText("monster").split(",");
+			LinkedList<Monster> list2 = new LinkedList<>();
+			for (String monster : monsterId) {
+				Integer id = Integer.valueOf(monster);
+				Monster monster2 = monsterList.get(id - 1);
+				list2.add(monster2);
+			}
+			scene.setMonsterList(list2);
+			// 设置地图的连通
+			String[] Near = e.elementText("near").split(",");
+			for (String near : Near) {
+				Area.mp2[Integer.valueOf(e.elementText("id"))][Integer.valueOf(near)] = 1;
+			}
+			//
+			Area.mp1.put(e.elementText("name"), Integer.valueOf(e.elementText("id")));
+			Area.sceneList.add(scene);
+		}
 	}
 
 	public static void init() {
@@ -31,8 +114,8 @@ public class Main {
 		Monster SLMmonster = new Monster();
 		SLMmonster.setName("史莱姆");
 		SLMmonster.setAliveFlag(true);
-		SLMmonster.setHp(100);
-		SLMmonster.setAck(20);
+		SLMmonster.setHp(5000);
+		SLMmonster.setAck(1);
 		Monster MDSmonster = new Monster();
 		MDSmonster.setName("美杜莎");
 		MDSmonster.setAliveFlag(true);
@@ -73,18 +156,18 @@ public class Main {
 		chenBao.setNpcList(list1);
 		chenBao.setMonsterList(list2);
 		Area.sceneList.add(chenBao);
-		
+
 		Skill skill = new Skill();
 		skill.setId(1);
 		skill.setName("毒雷引爆");
-		skill.setCd(3);
+		skill.setCd(3000);
 		skill.setMp(10);
-		skill.setHurt(30);
+		skill.setHurt(5);
 		skill.setEffect("中毒");
 		Skill sl = new Skill();
 		sl.setId(2);
 		sl.setName("神罗天罡");
-		sl.setCd(5);
+		sl.setCd(5000);
 		sl.setMp(20);
 		sl.setHurt(60);
 		sl.setEffect("护盾");
